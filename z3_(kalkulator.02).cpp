a35d25cc91a3a74b1d2812d386300e4ac49e4bd7
Karol Klasicki <287140@student.pwr.edu.pl>
2025-04-01 14:30:24 +0200

    Update z3_(kalkulator.02).cpp

--- z3_(kalkulator.02).cpp
+++ z3_(kalkulator.02).cpp
@@ -1,4 +1,3 @@
-﻿
 #include <iostream>
 
 class kalkulator {
@@ -9,10 +8,10 @@ public:
 	int n;
 	bool pam_u = 0;
 	bool pam_un;
-	int i = 0;
 	double a, b;
 
 private:
+	int i = 0;
 
     double dodawanie(double a, double b)
     {
@@ -29,6 +28,7 @@ private:
 			std::cout << pam << std::endl;
 			pam_un = 1;
 		}
+		return pam;
    }
 	double odejmowanie(double a, double b)
 	{
@@ -45,6 +45,7 @@ private:
 			std::cout << pam << std::endl;
 			pam_un = 1;
 		}
+		return pam;
 	}
 	double mnozenie(double a, double b)
 	{
@@ -61,6 +62,7 @@ private:
 			std::cout << pam << std::endl;
 			pam_un = 1;
 		}
+		return pam;
 	}
 	double dzielenie(double a, double b)
 	{
@@ -77,6 +79,7 @@ private:
 			std::cout << pam << std::endl;
 			pam_un = 1;
 		}
+		return pam;
 	}
 	int mod(int a, int b)
 	{
@@ -94,20 +97,34 @@ private:
 			std::cout << pam << std::endl;
 			pam_un = 1;
 		}
+		return pam;
 	}
-	int interfejs(int n) {
-		std::cout << "1)+  " << "2)-  " << "3)*  " << "4)/  " << "5)mod  " << "6) out  " << "7) m+  " << "8) m-  " << std::endl;
-		std::cin >> n;
-		if (n == 6) { i = i + 1; }
-		if (n == 7) { pam_u = 1; }
-		if (n == 8) { pam_u = 0; }
+public:
+	void interfejs() {
+		while (true) {
+			std::cout << "1)+  " << "2)-  " << "3)*  " << "4)/  " << "5)mod  " << "6) out  " << "7) m+  " << "8) m-  " << std::endl;
+			std::cin >> n;
+			if (n == 1) { dodawanie(0, 0); }
+			else if (n == 2) { odejmowanie(0, 0); }
+			else if (n == 3) { mnozenie(0, 0); }
+			else if (n == 4) { dzielenie(0, 0); }
+			else if (n == 5) { mod(0, 0); }
+			else if (n == 6) break;
+			else if (n == 7) { pam_u = 1; }
+			else if (n == 8) { pam_u = 0; }
+		}
 	}
+	
 
 };
 
 int main()
 {
-	
+	kalkulator calcu;
+	calcu.interfejs();
+	return 0;
+
     
 }
+
 
